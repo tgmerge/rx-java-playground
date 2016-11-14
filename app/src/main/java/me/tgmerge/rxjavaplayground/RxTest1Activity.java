b78fac623d5f05cbb8d97a999daee1f67a880624
tgmerge <tgmgsp@gmail.com>
2016-11-14 13:02:05 +0800

    update

--- app/src/main/java/me/tgmerge/rxjavaplayground/RxTest1Activity.java
+++ app/src/main/java/me/tgmerge/rxjavaplayground/RxTest1Activity.java
@@ -4,8 +4,15 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
 
 public class RxTest1Activity extends Activity {
+
+    private static final String TAG = RxTest1Activity.class.getSimpleName();
 
     public static void start(Context context) {
         Intent starter = new Intent(context, RxTest1Activity.class);
@@ -16,5 +23,64 @@ public class RxTest1Activity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rx_test1);
+
+        // Code 1 - 创建 Observer
+        Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onCompleted() {
+                Log.d(TAG, "Completed!");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "Error!");
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, "onNext: item: " + s);
+            }
+        };
+        // ---
+
+        // Code 2 - 创建 Subscriber
+        Subscriber<String> subscriber = new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+                Log.d(TAG, "onCompleted - subscriber");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "onError - subscriber");
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, "onNext: item: " + s);
+            }
+        };
+        // ---
+
+        // Code 3 - 创建 Observable
+        Observable observable = Observable.create(new Observable.OnSubscribe<String>() {
+
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("Hello");
+                subscriber.onNext("Hi");
+                subscriber.onCompleted();
+            }
+        });
+
+        Observable observable2 = Observable.just("Hello2", "Hi2", "Hi2 - again");
+
+        String[] words = {"Hello3", "Hi3", "Hi3 - again"};
+        Observable observable3 = Observable.from(words);
+        // ---
+
+        // Code 4 - 订阅
+        observable.subscribe(subscriber);
+
     }
 }
---
+++ note/1110 - RxJava 笔记.md
@@ -0,0 +1,48 @@
+# RxJava 笔记
+
+阅读内容： <http://gank.io/post/560e15be2dca930e00da1083>
+
+## 概念：扩展的观察者模式
+
+RxJava 是一个观察者模式的工具库。
+
+RxJava 中的基本概念：
+
+1. Observable - 被观察者
+2. Observer - 观察者
+3. subscribe - 订阅
+4. 事件
+
+RxJava 的事件回调，最基本的事件是`onNext()`，它相当于`onClick()`和`onEvent()`之类。
+
+特殊的事件：表示事件队列完结的`onCompleted()`和表示事件队列异常的`onError()`，正确运行的事件队列中，他们有且仅有一个（不是各有一个），且是事件序列中的最后一个。
+
+## 基本实现
+
+创建`Observer`（观察者） - Code 1
+
+创建`Subscriber` - Code 2
+
+`Subscriber`扩展了`Observer`，实际使用 subscribe 时，`Observer`会被转换成`Subscriber`。但`Subscriber`增加了
+
+1. `onStart()`方法：可选，总在 subscribe 发生的线程被调用，如果需要特定线程可以用`doOnSubscribe()`；
+2. `unsubscribe()`方法：取消订阅，不再接收事件。subscribe 之后，`Observable`会持有`Subscriber`的引用，不需要时应当尽快 unsubscribe 防止内存泄漏。
+
+创建`Observable`（被观察者） - Code 3
+
+`Observable.OnSubscribe` - 当 `Observable` 被订阅的时候将被调用的方法（这个对象的`call(...)`方法）。
+
+订阅 - Code 4
+
+订阅是使用 `observable.subscribe(subscriber)`，看起来是“给被观察者设置了一个订阅者”。
+
+`subscribe()`大致上会执行这几个操作：
+
+```java
+    public Subscription subscribe(Subscriber subscriber) {
+        subscriber.onStart();
+        onSubscribe.call(subscriber);    // 这个 onSubscribe 是 observable 的
+        return subscriber;
+    }
+```
+
